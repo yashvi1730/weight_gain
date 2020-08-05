e859a9ba357ef359bcc2deea68f8b19eebcb5765
Yashvi1730 <171220055@nitdelhi.ac.in>
2020-08-05 00:31:11 -0700

    changes 3

--- main/urls.py
+++ main/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from main import views
 
 urlpatterns=[
-    path('homepage',views.homepage,name="homepage"),
-    path('output',views.output,name="output"),
-    path('doctor_output',views.doctor_output,name="doctor_output"),
+    path('www.proactiveforher.com/tools/weight-gain-patient-information/input',views.homepage,name="homepage"),
+    path('www.proactiveforher.com/tools/weight-gain-patient-information/output',views.output,name="output"),
+    path('www.proactiveforher.com/tools/access-weight-gain-patient-information',views.doctor_output,name="doctor_output"),
     path('response',views.response,name="response"),
 ]
